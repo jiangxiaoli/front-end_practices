85d06180260b64223fa156d91035787e1e8d88de
jiangxiaoli <jiangxiaoli1104@gmail.com>
2014-07-06 00:00:13 -0700

    Ajax level1 - first Ajax call

--- AJAX/src/application.js
+++ AJAX/src/application.js
@@ -9,8 +9,24 @@ $(document).ready(function(){
 
 //show the hidden element when the button is clicked
 $(".confirmation").on("click","button",function(){
-	$(this).find(".ticket").slideDown();
+
+	//$.ajax(url[, setings])
+		//relative URL
+	$.ajax("confirmation.html",{
+		//runs only when server returns a successful response
+		success: function(response) {
+			$(".ticket").html(response).slideDown();
+		}
+
+	});
+
+	//same as $.get(url, success)	
+	/*$.get("confirmation.html", function(response) {
+		$(".ticket").html(response).slideDown();
+	});*/
+	
 });
+
 
 //show boarding pass when link is clicked
 $(".confirmation .view-boarding-pass").on("click",function(){
