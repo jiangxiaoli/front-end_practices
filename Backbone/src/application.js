86d7551f99d569354e0031f08989676b755471cf
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2014-07-18 22:41:49 -0700

    Update application.js

---
+++ Backbone/src/application.js
@@ -0,0 +1,7 @@
+var TodoItem = Backbone.Model.extend({});
+
+var TodoView = Backbone.View.extend({});
+
+var TodoList = Backbone.Collection.extend({});
+
+var app = new (Backbone.Router.extend({}));
